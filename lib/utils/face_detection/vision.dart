e80dc267f80d46fb19aad90536ea9fed6bf06c63
RungFif <rafifpranaja301@gmail.com>
2025-11-21 12:46:59 +0700

    feat: Add ML Kit face detection utility helpers

---
+++ lib/utils/face_detection/vision.dart
@@ -0,0 +1,5 @@
+// This file is no longer needed as we use google_mlkit_face_detection directly
+// Kept for reference only
+
+// Face recognition using Google ML Kit
+// Note: Direct usage of FaceDetector from google_mlkit_face_detection package
